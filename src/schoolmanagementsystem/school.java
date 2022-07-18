ff85562c32a3aedbadc7b7f5e50fb27ff20ef4ab
William Ford <96089722+wmaf@users.noreply.github.com>
2022-07-17 21:51:53 -0400

    issue with teacher

--- src/schoolmanagementsystem/school.java
+++ src/schoolmanagementsystem/school.java
@@ -1,7 +1,35 @@
 package schoolmanagementsystem;
 
-public class school {
-    private teacher teacher;
-    private students students;
+import java.util.List;
 
+public class school {
+    private List <teacher> teachers;
+    private List<students> students;
+    private int totalMoneyEarned = 0;
+    private int totalMoneySpent = 0;
+
+    public school(List<teacher> teachers, List<schoolmanagementsystem.students> students, int totalMoneyEarned, int totalMoneySpent) {
+        this.teachers = teachers;
+        this.students = students;
+        this.totalMoneyEarned = totalMoneyEarned;
+        this.totalMoneySpent = totalMoneySpent;
+
+    }
+
+    public List<teacher> getTeachers() {
+        return teachers;
+    }
+
+    public List<schoolmanagementsystem.students> getStudents() {
+        return students;
+    }
+
+    public int getTotalMoneyEarned() {
+        return totalMoneyEarned;
+    }
+
+    public int getTotalMoneySpent() {
+        return totalMoneySpent;
+    }
 }
+
